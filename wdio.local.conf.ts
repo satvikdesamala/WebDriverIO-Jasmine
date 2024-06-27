e35e3a725b6b3336443c4f69ba10e470ca9c0445
Satvik Desamala <158106295+satvikdesamala@users.noreply.github.com>
2024-06-27 13:08:10 -0500

    Add files via upload

---
+++ wdio.local.conf.ts
@@ -0,0 +1,17 @@
+import { config as sharedConfig } from "./wdio.shared.conf.js";
+
+export const config: WebdriverIO.Config = {
+    ...sharedConfig,
+    ...{
+        capabilities: [
+            {
+                browserName: "chrome",
+                "goog:chromeOptions": {
+                    // args: ['headless', 'disable-gpu']
+                    args: ["disable-gpu"],
+                },
+            },
+        ],
+        
+    },
+};
